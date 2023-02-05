62c77682b922a776ed1a700aca2e32c3b474e42a
Agung Wicaksono S <wicaksono1404@gmail.com>
2023-02-05 08:54:50 +0700

    Adding Policy Detail

---
+++ controllers/cxl/policyDetailController.js
@@ -0,0 +1,148 @@
+"use strict"
+
+const chalk = require('chalk');
+const { request } = require('express');
+const db = require('../../config/sequelize');
+const msCxlPolicyDetailModel = db.cxlPolicy;
+const AppError = require('../../utils/appError')
+
+
+const addMsCxlPolicyDetails = async (req, res) => {
+    // Extract userId from JWT token
+    const userId = req.user.id;
+
+    if (req.body && Array.isArray(req.body)) {
+        const datas = req.body.map(
+            response => {
+                return {
+                    cxlPolicyId: response.cxlPolicyId,
+                    description: response.description,
+                    cutOffDay: response.cutOffDay,
+                    time: response.description,
+                    chargeType: response.chargeType,
+                    value: response.value,
+                    status: 1,
+                    createdBy: userId,
+                }
+            });
+        await msCxlPolicyDetailModel.bulkCreate(datas).then(data => {
+            res.status(201).send({ data: data, message: 'Data created successfully' });
+        })
+            .catch(err => {
+                res.status(500).send({
+                    data: null,
+                    message:
+                        err.message =
+                        "Validation Error" ? err.message : "Some error occurred while creating the data."
+                });
+            });
+    } else {
+        res.status(500).send({ message: 'Bad Request Check Your Request' });
+    }
+}
+
+const getMsCxlPolicyDetails = async (req, res, next) => {
+    if (!req.query.size || !req.query.page) return res.status(500).send({ message: 'page number and page size are required !' })
+    let pageSize = +req.query.size;
+    if (pageSize > 100) {
+        pageSize = 100;
+    }
+    let pageOffset = ((+req.query.page - 1) * +req.query.size);
+    const data = await msCxlPolicyDetailModel.findAll({
+        data: [
+            'id',
+            'cxlPolicyId',
+            'description',
+            'cutOffDay',
+            'time',
+            'chargeType',
+            'value',
+            'status'
+        ],
+        offset: pageOffset,
+        limit: pageSize,
+    });
+
+    if (data.length > 0) {
+        res.status(200).send({ message: 'Success.', data: data });
+    } else {
+        res.status(404).send({ message: 'The data is not found.' });
+    }
+
+}
+
+const getMsCxlPolicyDetail = async (req, res) => {
+    const id = req.params.id;
+    const data = await msCxlPolicyDetailModel.findOne({
+        attributes: [
+            'id',
+            'cxlPolicyId',
+            'description',
+            'cutOffDay',
+            'time',
+            'chargeType',
+            'value',
+            'status'
+        ],
+        where: {
+            id: id
+        }
+    });
+
+    if (data) {
+        res.status(200).send({ message: 'Success.', data: data });
+    } else {
+        res.status(404).send({ message: 'The data is not found' });
+    }
+
+}
+
+const editMsCxlPolicyDetail = async (req, res) => {
+    const id = req.params.id;
+    // Extract userId from JWT token
+    const userId = req.user.id;
+    if (req.body && Array.isArray(req.body)) {
+        const currencies = req.body.map(
+            response => {
+                return {
+                    cxlPolicyId: response.cxlPolicyId,
+                    description: response.description,
+                    cutOffDay: response.cutOffDay,
+                    time: response.description,
+                    chargeType: response.chargeType,
+                    value: response.value,
+                    status: response.status,
+                    updatedBy: userId,
+                }
+            });
+        const updatedData = await msCxlPolicyDetailModel.update(currencies, { where: { id: id } });
+
+        if (updatedData[0] > 0) {
+            res.status(200).send({ message: 'Success Updated the data.', data: updatedData });
+        } else {
+            res.status(404).send({ message: 'Could not update the data.' });
+        }
+    } else {
+        res.status(500).send({ message: 'Bad Request Check Your Request' });
+    }
+}
+
+const deleteMsCxlPolicyDetail = async (req, res) => {
+    const id = req.params.id;
+    const deletedData = await msCxlPolicyDetailModel.destroy({ where: { id: id } });
+
+    if (deletedData) {
+        res.status(200).send({ message: 'The data deleted successfully.', data: deletedData });
+    } else {
+        res.status(404).send({ message: 'The data not deleted.' });
+    }
+}
+
+
+module.exports = {
+    addMsCxlPolicyDetails,
+    getMsCxlPolicyDetails,
+    getMsCxlPolicyDetail,
+    editMsCxlPolicyDetail,
+    deleteMsCxlPolicyDetail
+}
