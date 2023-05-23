3a46fafab7af3cdcb8469cf1cdff0c637032aa58
Ammar Farghani <ammarfarghani@Ammars-MacBook-Air.local>
2023-05-23 22:07:39 +0700

    rmv express validator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,15 @@
 "use strict"
-const { check, body, validationResult } = require('express-validator/check');
 const userController = require('./../controllers/users/userController');
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 
-router.post('/signup',
-    [
-        check('email')
-            .isEmail()
-            .withMessage('Please enter a valid email address.'),
-
-        body('password')
-            .isLength({ min: 5 })
-            .withMessage('Password length must be at least 5 chars.'),
-    ],
-    userController.createUser);
+router.post('/signup', userController.createUser);
 
 router.post('/login', userController.loginUser);
 
 router.post('/loginAdmin', userController.loginAdmin);
 
-router.post('/verifyUserToken',userController.verifyToken)
+router.post('/verifyUserToken', userController.verifyToken)
 
 // router.post('/verifyUserOtp',userController.verifyToken)
 
