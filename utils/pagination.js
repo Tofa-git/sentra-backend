2e6c104939228a9f8179984c5037d6fdbe75954f
Ammar Farghani <ammarfarghani@Ammars-MacBook-Air.local>
2023-06-03 16:56:08 +0700

    fix pagination

--- utils/pagination.js
+++ utils/pagination.js
@@ -3,7 +3,7 @@ module.exports = {
     query.limit = req.query.limit ? +req.query.limit : 10
     query.page = req.query.page ? +req.query.page : 1
     query.totalPage = Math.ceil(query.count / (req.query.limit ? +req.query.limit : 10))
-    query.hasNext = query.page === query.totalPage ? false : true
+    query.hasNext = query.totalPage === 0 ? false : query.page === query.totalPage ? false : true
     query.hasPreviouse = query.page === 1 ? false : true
 
     return query;
