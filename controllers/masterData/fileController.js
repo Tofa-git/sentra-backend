61f7ae2d6a46260c537e5838d33ebb1ce089cd75
Agung Wicaksono S <wicaksono1404@gmail.com>
2023-06-20 09:59:48 +0700

    Fixing File

--- controllers/masterData/fileController.js
+++ controllers/masterData/fileController.js
@@ -1,7 +1,6 @@
 "use strict"
 
 const fs = require('fs');
-const path = require('path');
 const db = require('../../config/sequelize');
 const { responseSuccess, responseError } = require('../../utils/response');
 const fileModel = db.file;
